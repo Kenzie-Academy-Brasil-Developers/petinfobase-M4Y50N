c8597b4e3edc694f43eb733a7545a28f62fdc3b2
M4Y50N <pethersonreis@gmail.com>
2022-10-29 17:13:38 -0300

    feat: loading button

--- scripts/inputLoading.js
+++ scripts/inputLoading.js
@@ -1,14 +1,41 @@
 //Verify input emptyness
-function isInputDisable() {
-	const input = document.querySelector("#user");
+function isInputDisable(elements) {
+	elements = [...elements];
+	console.log(elements);
+	const filteredArray = elements.filter((elem) => {
+		return elem.tagName == "INPUT";
+	});
 
-	input.addEventListener("input", () => {
-		const btn = document.querySelector("form button");
-		btn.classList.remove("disable");
-		if (input.value == "") {
-			btn.classList.add("disable");
-		}
+	filteredArray.forEach((elem) => {
+		elem.addEventListener("input", () => {
+			const noEmp = filteredArray.filter((e) => {
+				return e.value == "";
+			});
+
+			const btn = document.querySelector("form button");
+			if (!noEmp.length) {
+				btn.classList.remove("disable");
+			} else {
+				btn.classList.add("disable");
+			}
+		});
 	});
 }
-//verify input
-isInputDisable();
+
+function loading() {
+	const load = document.querySelector(".loading");
+	const btn_sub = document.querySelector(".btn-submit");
+	const btn_sub_cont = document.querySelector(".btn-submit .content");
+
+	load.classList.remove("hidden");
+	btn_sub.classList.add("disable");
+	btn_sub_cont.classList.add("hidden");
+
+	setTimeout(() => {
+		load.classList.add("hidden");
+		btn_sub.classList.remove("disable");
+		btn_sub_cont.classList.remove("hidden");
+	}, 1000);
+}
+
+export { isInputDisable, loading };
--- scripts/login.js
+++ scripts/login.js
@@ -1,19 +1,25 @@
 import { login } from "./requests.js";
+import { isInputDisable, loading } from "./inputLoading.js";
 
 const eventLogin = () => {
 	const form = document.querySelector("form");
 	const elements = [...form.elements];
 
+	isInputDisable(form.elements);
+
 	form.addEventListener("submit", async (e) => {
 		e.preventDefault();
 
 		const body = {};
-
 		elements.forEach((elem) => {
 			if (elem.tagName === "INPUT" && elem.value !== "") {
 				body[elem.type] = elem.value;
 			}
 		});
+
+		isInputDisable(form.elements);
+
+		loading();
 
 		await login(body);
 	});
